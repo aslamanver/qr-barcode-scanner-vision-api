5bb96e6c8ac502e4eea7d5c7bd43f3f6d765f05c
Aslam Anver <slam.anver9@gmail.com>
2020-10-16 17:05:25 +0530

    Release-v1.1.1

--- app/src/main/java/com/payable/scan/SplashActivity.java
+++ app/src/main/java/com/payable/scan/SplashActivity.java
@@ -1,17 +1,15 @@
 package com.payable.scan;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
-
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.os.Handler;
-import android.view.Window;
 import android.view.WindowManager;
 import android.widget.TextView;
-import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 public class SplashActivity extends AppCompatActivity {
 
--- app/src/main/java/com/payable/scan/ZxingActivity.java
+++ app/src/main/java/com/payable/scan/ZxingActivity.java
@@ -4,10 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.util.Log;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.CompoundButton;
 import android.widget.Toast;
 
 import com.google.zxing.integration.android.IntentIntegrator;
@@ -20,47 +23,51 @@ public class ZxingActivity extends AppCompatActivity {
 
     ActivityZxingBinding binding;
     IntentIntegrator integrator;
+    SharedPreferences sharedPrefs;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = DataBindingUtil.setContentView(this, R.layout.activity_zxing);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        sharedPrefs = PreferenceManager.getDefaultSharedPreferences(this);
 
-        binding.btnScan.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+        if (sharedPrefs.getBoolean("startup_scanner", false)) openScanner();
+        binding.btnScan.setOnClickListener(v -> openScanner());
+        binding.checkboxStartup.setChecked(sharedPrefs.getBoolean("startup_scanner", false));
+        binding.checkboxStartup.setOnCheckedChangeListener((view, isChecked) -> sharedPrefs.edit().putBoolean("startup_scanner", isChecked).commit());
+    }
 
-                integrator = new IntentIntegrator(ZxingActivity.this);
-                integrator.setDesiredBarcodeFormats(IntentIntegrator.QR_CODE_TYPES);
-                integrator.setPrompt("Scan a QR Code");
-                integrator.setCameraId(0);
-                integrator.setBeepEnabled(true);
-                integrator.setBarcodeImageEnabled(true);
-                integrator.initiateScan();
-            }
-        });
+    private void openScanner() {
+        integrator = new IntentIntegrator(ZxingActivity.this);
+        integrator.setDesiredBarcodeFormats(IntentIntegrator.QR_CODE_TYPES);
+        integrator.setPrompt("Scan QR Code");
+        integrator.setCameraId(0);
+        integrator.setBeepEnabled(true);
+        integrator.setBarcodeImageEnabled(true);
+        integrator.initiateScan();
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-
         IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
-
         if (result != null) {
-
             if (result.getContents() != null) {
-
                 Intent intent = new Intent(this, SalesPadActivity.class);
                 intent.putExtra("scan_code", result.getContents());
-                // intent.putExtra("scan_amount", (double) new Random().nextInt(1500));
+                double amount;
+                try {
+                    String values[] = result.getContents().split(",");
+                    String value = values[values.length - 1];
+                    amount = Double.parseDouble(value);
+                } catch (ArrayIndexOutOfBoundsException | NumberFormatException ex) {
+                    amount = 0;
+                }
+                intent.putExtra("scan_amount", amount);
                 startActivity(intent);
-
                 Log.e("DarazScan", result.getContents());
             }
-
         } else {
-
             super.onActivityResult(requestCode, resultCode, data);
         }
     }
